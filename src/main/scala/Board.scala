11a0c5e1f4d6116e3f6057e41e86feb3c046fdda
Jimmy <jakubpleban297@gmail.com>
2018-05-19 19:08:53 +0200

    Test branch

---
+++ src/main/scala/Board.scala
@@ -0,0 +1,59 @@
+class Board {
+  var tab:Array[Array[Int]] = {
+    val t = Array(Array(0, 1, 0, 1, 0, 1, 0, 1),
+      Array(1, 0, 1, 0, 1, 0, 1, 0),
+      Array(0, 1, 0, 1, 0, 1, 0, 1),
+      Array(0, 0, 0, 0, 0, 0, 0, 0),
+      Array(0, 0, 0, 0, 0, 0, 0, 0),
+      Array(-1, 0,-1, 0,-1, 0,-1, 0),
+      Array(0,-1, 0,-1, 0,-1, 0,-1),
+      Array(-1, 0,-1, 0,-1, 0,-1, 0))
+    t
+  }
+
+  /*def initTab: Array[Array[Int]] = {
+    val t = Array(Array(0, 1, 0, 1, 0, 1, 0, 1),
+                  Array(1, 0, 1, 0, 1, 0, 1, 0),
+                  Array(0, 1, 0, 1, 0, 1, 0, 1),
+                  Array(0, 0, 0, 0, 0, 0, 0, 0),
+                  Array(0, 0, 0, 0, 0, 0, 0, 0),
+                  Array(-1, 0,-1, 0,-1, 0,-1, 0),
+                  Array(0,-1, 0,-1, 0,-1, 0,-1),
+                  Array(-1, 0,-1, 0,-1, 0,-1, 0))
+     t
+  }*/
+
+  def isChecker(x: Int, y: Int): Boolean = {
+    if(tab(x)(y) == 1 || tab(x)(y) == -1)
+      true
+    else
+      false
+  }
+
+  def move(x_start: Int, y_start: Int, x_end: Int, y_end: Int): Unit = {
+    if (isChecker(x_start, y_start)) {
+      val value = tab(x_start)(y_start)
+      tab(x_end)(y_end) = value
+      tab(x_start)(y_start) = 0
+    }
+  }
+
+  def value(x: Int, y: Int): Int = tab(x)(y)
+
+  def change(x: Int, y: Int, value: Int): Unit = tab(x)(y) = value
+
+  def king(x: Int, y: Int): Unit = {
+    if( isChecker(x, y) )
+      if( value(x, y) == 1 )
+        change(x, y, 2)
+      else
+        change(x, y, -2)
+  }
+
+  def color(x: Int, y: Int): String = {
+    if( value(x, y) > 0)
+      "white"
+    else
+      "black"
+  }
+}
